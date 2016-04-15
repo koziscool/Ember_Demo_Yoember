d7c1d25e03dcc778187255d532ae6be0c0622221
koziscool <kozjob@yahoo.com>
2016-04-15 11:58:48 -0500

    hw for lesson 3

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.createRecord('contact');
+  },
+
+  actions: {
+
+    sendMessage(newContactMessage) {
+      newContactMessage.save().then(() => this.controller.set('responseMessage', true));
+    },
+
+    willTransition() {
+      // rollbackAttributes() removes the record from the store
+      // if the model 'isNew'
+      this.controller.get('model').rollbackAttributes();
+    }
+  }
+
 });
