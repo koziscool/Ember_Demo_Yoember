a27d7eebbe825545cea62f9f7cc1fbbd98a6b5be
koziscool <kozjob@yahoo.com>
2016-04-15 13:34:26 -0500

    stuff

--- app/models/library.js
+++ app/models/library.js
@@ -4,7 +4,7 @@ import attr from 'ember-data/attr';
 import DS from 'ember-data';
 import Ember from 'ember';
 
-export default Model.extend({
+export default DS.Model.extend({
   name: DS.attr('string'),
   address: DS.attr('string'),
   phone: DS.attr('string'),
