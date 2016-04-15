ea386b72acc630af6ca5bb39d3aba3b79a525adc
koziscool <kozjob@yahoo.com>
2016-04-15 08:39:01 -0500

    stuff

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,5 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  isDisabled: true
+  isDisabled: true,
+
+  isDisabled: Ember.computed('emailAddress', function() {
+    return this.get('emailAddress') === '';
+  }),
+  
+  actualEmailAddress: Ember.computed('emailAddress', function() { 
+    console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
+  }),
+
+  emailAddressChanged: Ember.observer('emailAddress', function() { 
+    console.log('observer is called', this.get('emailAddress')); 
+  })
+
+
 });
